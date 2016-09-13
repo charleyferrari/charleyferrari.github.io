98791ef23cf844eadb314095487dc05fc8522c5c
Charley Ferrari <charley@charleyferrari.com>
2016-09-13 18:42:32 -0400

    adding images

--- agentscreate.R
+++ agentscreate.R
@@ -7,7 +7,7 @@ agentsraw <- read.csv("AgentsRaw.csv")
 
 futurelist <- c(colnames(agentsraw)[1:2],colnames(agentsraw)[14:length(colnames(agentsraw))])
 
-currentlist <- colnames(agentsraw)[1:13]
+currentlist <- c(colnames(agentsraw)[1:13],colnames(agentsraw[25]))
 
 agentsfuture <- agentsraw[,futurelist]
 agentscurrent <- agentsraw[,currentlist]
