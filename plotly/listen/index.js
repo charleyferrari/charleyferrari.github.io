5c73654cac64e9e0c8017e14d79575294c672780
Charley Ferrari <charley@charleyferrari.com>
2017-03-27 13:55:43 -0400

    Adding listen event

--- plotly/listen/index.js
+++ plotly/listen/index.js
@@ -13,3 +13,16 @@ window.addEventListener('message', function(e) {
     alert(message.type);
     console.log(message); // prints object for zoom, click, or hover event
 });
+
+var pinger = setInterval(function(){
+    plot.postMessage({task: 'ping'}, 'https://plot.ly')
+}, 100);
+
+window.addEventListener('message', function(e) {
+    var message = e.data;
+    if(message.pong && pinger !== null) {
+        clearInterval(pinger);
+        pinger = null;
+        plot.postMessage({task: 'listen', events: ['zoom', 'click', 'hover']}, 'https://plot.ly');
+    }
+});
