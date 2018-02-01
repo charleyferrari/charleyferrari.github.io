3ba47583af3bc28d01d62f853fa037ddbfb9f9ef
Charley Ferrari <charley@charleyferrari.com>
2018-02-01 15:22:03 -0500

    get rid of modebar

--- together.js
+++ together.js
@@ -756,7 +756,7 @@ function drawGoogleChart() {
         });
     });
 
-    Plotly.newPlot('GoogleChart', {data: [traces[0]], layout: layout, frames: frames}, {layout: layout}, {displayModeBar: false});
+    Plotly.newPlot('GoogleChart', {data: [traces[0]], layout: layout, frames: frames, config: {displayModeBar: false}});
 
     console.log(frames);
     // var chart = new google.visualization.MotionChart(document.getElementById('GoogleChart'));
