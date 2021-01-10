fb7e7759a342d2c04dfc9a66c65fef3c107212ca
Charley Ferrari <charley@charleyferrari.com>
2021-01-10 16:24:17 -0500

    working in javascript, test in githubio

---
+++ economicsobservatory/chart2.js
@@ -0,0 +1,37 @@
+Plotly.d3.csv('used_manchester.csv', d => {
+    console.log(d);
+    let x = [], y = [], color = [];
+
+    for (let i=0; i<d.length; i++){
+        row = d[i];
+        x.push(row['Area']);
+        y.push(row['Millions']);
+        color.push(row['fill']);
+    }
+
+    let data = [{
+        'x': x,
+        'y': y,
+        'width': 0.5,
+        'type': 'bar',
+        'marker': {
+            'color': color,
+        },
+        'hoverinfo': 'y'
+    }];
+
+    let layout = {
+        'plot_bgcolor': '#fff',
+        'yaxis': {
+            'gridcolor': 'rgb(238, 238, 238)',
+            'title': 'Population (Millions)'
+        }
+    };
+
+    let config = {
+        'modeBarButtonsToRemove': ['zoom2d', 'pan2d', 'select2d', 'lasso2d', 'zoomIn2d', 
+            'zoomOut2d', 'autoScale2d', 'resetScale2d', 'toggleHover', 'toggleSpikelines']
+    };
+
+    Plotly.newPlot('chart2', data, layout, config);
+});
---
+++ economicsobservatory/chart3.js
@@ -0,0 +1,61 @@
+Plotly.d3.csv('used_life_ex.csv', d => {
+    console.log(d);
+    let xbar = [], ybar = [], xline = [], yline;
+
+    for (let i=0; i<d.length; i++){
+        row = d[i];
+        if (row['AreaName'] != 'England') {
+            xbar.push(row['AreaName']);
+            ybar.push(row['Value']);
+            xline.push(row['AreaName']);
+        } else {
+            yline = Array(d.length).fill(row['Value']);
+        }
+    }
+
+    let data = [
+        {
+            'x': xbar,
+            'y': ybar,
+            'width': 0.5,
+            'type': 'bar',
+            'marker': {
+                'color': 'rgb(55,126,184)'
+            },
+            'showlegend': false,
+            'hoverinfo': 'y',
+        },
+        {
+            'x': xline,
+            'y': yline,
+            'type': 'scatter',
+            'line': {
+                'color': 'rgb(228,26,28)'
+            },
+            'mode': 'lines',
+            'hoverinfo': 'none',
+            'name': 'English Average'
+        }
+    ];
+
+    let layout = {
+        'plot_bgcolor': '#fff',
+        'yaxis': {
+            'gridcolor': 'rgb(238, 238, 238)',
+            'title': 'Healthy life expectancy at birth (male)'
+        },
+        'legend': {
+            'yanchor': 'bottom',
+            'y': -0.15,
+            'xanchor': 'right',
+            'x': 0.5
+        }
+    };
+
+    let config = {
+        'modeBarButtonsToRemove': ['zoom2d', 'pan2d', 'select2d', 'lasso2d', 'zoomIn2d', 
+            'zoomOut2d', 'autoScale2d', 'resetScale2d', 'toggleHover', 'toggleSpikelines']
+    };
+
+    Plotly.newPlot('chart3', data, layout, config);
+});
